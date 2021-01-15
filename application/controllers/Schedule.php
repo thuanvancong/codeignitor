4847c18d76b4a030c633148f102eaee6cca793de
thuanvc <thuanvancong121@gmail.com>
2021-01-15 23:48:39 +0700

    feature/1_created_schedule

--- application/controllers/Schedule.php
+++ application/controllers/Schedule.php
@@ -14,6 +14,56 @@ class Schedule extends Pageparent_Controller
    		// $table = 'class_by_student';
    		// $data['dbClassStudent'] = GetDBTable($table);
    		$data['pageName'] = 'schedule';
+      $this->load->helper('url');
+      $data['ajaxScheduleStudent'] =  site_url('Schedule/ajaxScheduleStudent');
    		$this->load->view("quanlytrungtam/layout",$data);
    	}
+
+    function ajaxScheduleStudent()
+    {
+      $student_identitycard = $_POST['schedule_student'];
+      // Lấy Thông Tin Theo CMND
+      $this->load->model("quanlytrungtam_model");
+      $data = $this->quanlytrungtam_model->getItemScheduleByIndentify($student_identitycard);
+      $mang = array();
+      $extend = array();
+      for ($i=0; $i < count($data); $i++)
+      {
+        $id_class_student = $data[$i]['class_student_id'];
+        $arrayStudentName = $this->quanlytrungtam_model->getStudentNameByID($data[$i]['student_id']);
+        $arrayClassName = $this->quanlytrungtam_model->getClassNameByID($data[$i]['class_id']);
+        $arrayShift = $this->quanlytrungtam_model->getShiftTimeByID($data[$i]['shift_id']);
+        $arrayTeacherName = $this->quanlytrungtam_model->getTeacherNameByID($data[$i]['teacher_id']);
+        // $extend['arrayStudentName'] = $arrayStudentName;
+        // $extend['arrayClassName'] = $arrayClassName;
+        // $extend['arrayShift'] = $arrayShift;
+        // $extend['arrayTeacherName'] = $arrayTeacherName;
+        $extend['arrayStudentName'] = $arrayStudentName[0];
+        $extend['arrayClassName'] = $arrayClassName[0];
+        $extend['arrayShift'] = $arrayShift[0];
+        $extend['arrayTeacherName'] = $arrayTeacherName[0];
+        $mang[] = $extend;
+      }
+      $ketquaAjax = array();
+      for ($i=0; $i < count($mang); $i++)
+      {
+        $student_name = $mang[$i]['arrayStudentName']['student_name'];
+        $class_name = $mang[$i]['arrayClassName']['class_name'];
+        $teacher_name = $mang[$i]['arrayTeacherName']['teacher_name'];
+        $time_in = $mang[$i]['arrayShift']['time_in'];
+        $time_out = $mang[$i]['arrayShift']['time_out'];
+        $mangExtend = array(
+          'student_name' => $student_name,
+          'class_name' => $class_name,
+          'teacher_name' => $teacher_name,
+          'time_in' => $time_in,
+          'time_out' => $time_out
+        );
+        $ketquaAjax[] = $mangExtend;
+      }
+      $ketqua = array(
+        'ketqua' => $ketquaAjax
+      );
+      echo json_encode($ketqua);
+    }
 }
--- application/views/quanlytrungtam/schedule.php
+++ application/views/quanlytrungtam/schedule.php
@@ -3,29 +3,105 @@
 		<div class="panel panel-info">
 			<div class="panel-heading">Lịch Học Các Lớp</div>
 			<div class="panel-body">
+				<div class="form-group">
+					<label>Chọn Chủ Đề</label>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadiosStudent" value="student" checked="">Thời Khóa Biểu Theo Học Sinh
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadiosClass" value="class" checked="">Thời Khóa Biểu Lớp
+						</label>
+					</div>
+				</div>
 				<div class="row">
-					<!-- <?php 
-						echo '<table style="width:90%" id="table-role" class="table table-striped">
-						  <tr>
-						    <th>ID</th>
-						    <th>role</th> 
-						    <th>Active</th>
-						    <th>Router</th>
-						  </tr>';
-						foreach ($dbClassStudent as $key => $value) {
-							echo 
-							'
-							<tr class="role-item" id="item'.$value['role_id'].'">
-								<td id="role_id'.$value['role_id'].'" class="role_id">'.$value['role_id'].'</td>
-								<td id="role_name'.$value['role_id'].'" class="role_name">'.$value['role_name'].'</td>
-								<td id="role_isactive'.$value['role_id'].'" class="role_isactive">'.$value['role_isactive'].'</td>
-								<td id="router'.$value['role_id'].'" class="router">'.$value['router'].'</td>';
-						}
-						echo'</tr>';
-						echo'</table>';
-					?> -->
+					<div class="input-group hidden" id="schedule-student">
+						<input id="input-ID-student" type="number" class="form-control input-md" placeholder="Nhập CMND Sinh Vien">
+						<span class="input-group-btn">
+							<button class="btn btn-primary btn-md" id="btn-chat" onclick="ajaxScheduleStudent()" >GỬI</button>
+						</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-group hidden" id="schedule-class">
+						<input id="input-ID-class" type="text" class="form-control input-md" placeholder="Nhập Lớp">
+						<span class="input-group-btn">
+							<button class="btn btn-primary btn-md" id="btn-chat" onclick="AjaxScheduleClass()">GỬI</button>
+						</span>
+					</div>
+				</div>
+				<div class="row">
+					<table style="width:100%" id="table-schedule-student" class="table table-striped hidden">
+						<thead>
+							<tr>
+							    <th>Lớp</th>
+							    <th>Tên Học Viên</th> 
+							    <th>Giáo Viên Phụ Trách</th>
+							    <th>Thời Gian Vào</th>
+							    <th>Thời Gian Ra</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$(document).ready(
+		function(){
+			$( "input" ).on( "click", function() {
+				$('#schedule-student').addClass('hidden');
+				$('#schedule-class').addClass('hidden');
+			  	var key = $( "input:checked" ).val();
+			  	showScheduleByRadio(key);
+			});
+		}
+	);
+
+	function showScheduleByRadio(key_radio)
+	{
+		if(key_radio == 'student')
+		{
+			$('#schedule-student').removeClass('hidden');
+		}
+		if(key_radio == 'class')
+		{
+			$('#schedule-class').removeClass('hidden');
+		}
+	}
+
+	function ajaxScheduleStudent()
+	{
+		var schedule_student = $('#input-ID-student').val();
+		$.ajax({
+			method: "POST",
+			url: '<?php echo $ajaxScheduleStudent; ?>', 
+			dataType:'json',
+			data:{schedule_student:schedule_student}
+		}).done(function(data){
+			var ketqua = data.ketqua,
+				htmlString = '';
+			$('#table-schedule-student').removeClass('hidden');
+			var i;
+			for(i = 0;i < ketqua.length;i++)
+			{
+				htmlString = '<tr><td id="schedule_class_id"'+ketqua[i].student_id+'>'+ketqua[i].class_name+'</td><td id="schedule_student_id"'+ketqua[i].student_id+'>'+ketqua[i].student_name+'</td><td id="schedule_student_id"'+ketqua[i].student_id+'>'+ketqua[i].teacher_name+'</td><td id="schedule_student_id"'+ketqua[i].student_id+'>'+ketqua[i].time_in+'</td><td id="schedule_student_id"'+ketqua[i].student_id+'>'+ketqua[i].time_out+'</td></tr>';
+				$('#table-schedule-student').find('tbody').append(htmlString);
+			}
+		});
+	}
+
+	function AjaxScheduleClass()
+	{
+		var schedule_class = $('#input-ID-class').val();
+	}
+</script>
